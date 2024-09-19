75f4d6427c97c4fecce4f75fe39694141fca1cac
Mainak Karmakar <iammainak58@gmail.com>
2024-09-19 19:31:32 +0000

    Variables, Data Types, and Basic Operations

---
+++ 01basic.js
@@ -0,0 +1,58 @@
+// const let var
+
+
+// console.log("youtube")
+
+// const x = 10
+
+// const yourName = "mainak"
+// console.log(yourName)
+// console.log("yourName")
+
+// const x = 15
+// console.log(x)
+// x= 10
+// console.log(x) 
+
+// let y = 10
+// console.log(y)
+// y = 15
+// console.log(y)
+
+// {
+//     var x =  10
+//     console.log(x)
+// }
+// console.log(x)
+
+// const x = "mainak"
+// const y = 10
+// console.log(typeof x)
+// console.log(typeof y)
+//  Boolean
+
+// let x = 10
+// let y = 15
+// let z = 10
+// let a = "10"
+
+// console.log(x == y)
+// console.log(x == z)
+// console.log(x !== z)
+
+// const student = {
+//     name: "mainak",
+//     roll: 1,
+//     class:5
+// }
+
+// student.roll = 5
+// console.log(student.roll)
+
+// let x = 10
+// let y = 15
+
+// let z = x+y
+
+// console.log(z)
+// console.log(x/y)
